a382fae7f3caf6e766b37cfa99ca50ff81f92c66
Saucedechoux <saucedechoux@gmail.com>
2021-09-18 21:19:02 +0200

    Améliorations header + meilleur support mobile

--- js/app.js
+++ js/app.js
@@ -1,14 +1,23 @@
 "use strict";
 const slider = document.querySelector(".slideshow-container");
-const parallax = document.querySelector(".thumbnail");
+const title = document.querySelector(".title");
+const header = document.querySelector(".header");
 let holding = false;
 let firstClickX;
 let alreadyLeftScrolled;
+window.addEventListener("scroll", () => {
+    let rect = title === null || title === void 0 ? void 0 : title.getBoundingClientRect();
+    if (rect.top <= 100 && window.innerWidth > 565) {
+        header.classList.add("scroll");
+    }
+    else {
+        header.classList.remove("scroll");
+    }
+});
 slider.addEventListener("mousedown", e => {
     holding = true;
     firstClickX = e.pageX - slider.offsetLeft;
     alreadyLeftScrolled = slider.scrollLeft;
-    console.log(alreadyLeftScrolled);
 });
 slider.addEventListener("mousemove", e => {
     if (!holding)
@@ -22,4 +31,19 @@ slider.addEventListener("mouseup", () => {
 });
 slider.addEventListener("mouseleave", () => {
     holding = false;
+});
+slider.addEventListener("touchstart", e => {
+    holding = true;
+    firstClickX = e.targetTouches[0].pageX - slider.offsetLeft;
+    alreadyLeftScrolled = slider.scrollLeft;
+});
+slider.addEventListener("touchend", () => {
+    holding = false;
+});
+slider.addEventListener("touchmove", e => {
+    if (!holding)
+        return;
+    const x = e.targetTouches[0].pageX - slider.offsetLeft;
+    const scrolled = (x - firstClickX) * 1.5;
+    slider.scrollLeft = alreadyLeftScrolled - scrolled;
 });
--- ts/app.ts
+++ ts/app.ts
@@ -1,16 +1,25 @@
 const slider = document.querySelector(".slideshow-container") as HTMLDivElement;
-const parallax = document.querySelector(".thumbnail") as HTMLImageElement;
+const title = document.querySelector(".title") as HTMLHeadingElement;
+const header = document.querySelector(".header") as HTMLDivElement;
 
 let holding:boolean = false;
 let firstClickX:number;
-let alreadyLeftScrolled:number; 
+let alreadyLeftScrolled:number;
+
+window.addEventListener("scroll", () => {
+    let rect:DOMRect = title?.getBoundingClientRect();
+    if (rect.top <= 100 && window.innerWidth > 565) {
+        header.classList.add("scroll")
+    } else {
+        header.classList.remove("scroll")
+    }
+    
+})
 
 slider.addEventListener("mousedown", e => {
     holding = true;
     firstClickX = e.pageX - slider.offsetLeft;
     alreadyLeftScrolled = slider.scrollLeft;
-    console.log(alreadyLeftScrolled);
-    
 })
 
 slider.addEventListener("mousemove", e => {
@@ -27,4 +36,21 @@ slider.addEventListener("mouseup", () => {
 
 slider.addEventListener("mouseleave", () => {
     holding = false;
+})
+
+slider.addEventListener("touchstart", e => {
+    holding = true;
+    firstClickX = e.targetTouches[0].pageX - slider.offsetLeft;
+    alreadyLeftScrolled = slider.scrollLeft;
+})
+
+slider.addEventListener("touchend", () => {
+    holding = false;
+})
+
+slider.addEventListener("touchmove", e => {
+    if (!holding) return;
+    const x = e.targetTouches[0].pageX - slider.offsetLeft;
+    const scrolled = (x - firstClickX) * 1.5;
+    slider.scrollLeft = alreadyLeftScrolled - scrolled;
 })
